705f8d3ddf6056b115ec34aae39e12e3c96b14f7
chh134159 <872544926@qq.com>
2018-04-02 13:38:48 +0800

    修改样式表

--- src/App.vue
+++ src/App.vue
@@ -12,34 +12,5 @@ export default {
 </script>
 
 <style>
-body {
-    line-height: 1.6;
-    font-family: 'Microsoft Yahei',-apple-system-font,Helvetica Neue,sans-serif;
-}
-#app {
-  /* font-family: 'Droidsansfallback','Avenir', Helvetica, Arial, sans-serif; */
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-size: 14px;
-  margin: 0;
-  padding: 0;
-  
-}
-* {
-  margin: 0;
-  padding: 0;
-}
-ul {
-    margin: 0;
-    padding: 0;
-  }
-  /* 清楚浮动 */ 
-.clearfloat:after{
-  display:block;
-  clear:both;
-  content:"";
-  visibility:hidden;
-  height:0
-} 
-.clearfloat{zoom:1} 
+
 </style>
